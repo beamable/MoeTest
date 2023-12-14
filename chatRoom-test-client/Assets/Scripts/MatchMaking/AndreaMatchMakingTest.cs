2be4c01120a256087436640ba5782e35a5dca7d0
NovusX-Dev <152214057+mhijaziB@users.noreply.github.com>
2023-12-14 14:36:27 -0300

    Andrea Random match making

--- chatRoom-test-client/Assets/Scripts/MatchMaking/AndreaMatchMakingTest.cs
+++ chatRoom-test-client/Assets/Scripts/MatchMaking/AndreaMatchMakingTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Beamable;
 using Beamable.Common.Content;
@@ -8,6 +9,8 @@ using Helper;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace MatchMaking
 {
@@ -40,12 +43,18 @@ namespace MatchMaking
         [SerializeField] private SimGameTypeRef gameTypeRef = null;
         [SerializeField] private string powerStatString = "power";
         [SerializeField] private TMP_InputField powerStatValueInputField = null;
+        [SerializeField] private MyDebugger myDebugger = null;
+        
+        [Header("Random Match Making")]
+        [SerializeField] private Toggle useRandomMatchToggle = null;
+        [SerializeField] private SimGameTypeRef[] randomMatchRefs = null;
+        [SerializeField] private TMP_InputField fieldSizeInputField = null;
+        [SerializeField] private TextMeshProUGUI randomMatchNameText = null;
         
         #endregion
 
         #region PRIVATE_VARIABLES
         
-        private MyDebugger _myDebugger = null;
         private BeamContext _beamContext;
         private GameServerClient _gameServerClient = null;
         private MyMatchMaking _myMatchmaking = null;
@@ -55,6 +64,8 @@ namespace MatchMaking
         #endregion
 
         #region PUBLIC_VARIABLES
+        
+        private bool _useRandomMatch = false;
 
         #endregion
 
@@ -62,27 +73,32 @@ namespace MatchMaking
         
         private void Start()
         {
-            _myDebugger = FindObjectOfType<MyDebugger>();
-            SetupBeamable();
+            myDebugger.SimpleDebug($"Check the Random Match Toggle then SetupBeamable to start...");
+            // SetupBeamable();
         }
 
         #endregion
 
         #region PRIVATE_METHODS
 
-        private async void SetupBeamable()
+        public async void SetupBeamable()
         {
-            _myDebugger.SimpleDebug("Beamable Setup Start...");
+            _useRandomMatch = useRandomMatchToggle.isOn;
+            myDebugger.SimpleDebug("Beamable Setup Start...");
             _beamContext = await BeamContext.Default.Instance;
-            _simGameType = await gameTypeRef.Resolve();
-            _data.SessionState = SessionState.Disconnected;
-            _myDebugger.SimpleDebug($"Beamable Setup Complete!");
-            _myDebugger.SimpleDebug($"Player Id = {_beamContext.PlayerId}");
+            myDebugger.SimpleDebug($"Beamable Setup Complete!");
+            myDebugger.SimpleDebug($"Player Id = {_beamContext.PlayerId}");
+            if(_useRandomMatch) return;
+            SimGameTypeRef gameTypeRefToUse = null;
+            gameTypeRefToUse = _useRandomMatch ? GetRandomMatchRef() : gameTypeRef;
+            _simGameType = await gameTypeRefToUse.Resolve();
             
+            _data.SessionState = SessionState.Disconnected;
+            myDebugger.SimpleDebug($"Game Type = {_simGameType.Id}");
             _myMatchmaking = new MyMatchMaking(_beamContext.Api.Experimental.MatchmakingService, 
                 _simGameType,
                 _beamContext.PlayerId,
-                _myDebugger);
+                myDebugger);
             
             _myMatchmaking.OnProgress.AddListener(MyMatchMaking_OnProgress);
             _myMatchmaking.OnComplete.AddListener(MyMatchMaking_OnComplete);
@@ -90,20 +106,51 @@ namespace MatchMaking
             
         }
         
+        private SimGameTypeRef GetRandomMatchRef()
+        {
+            if(!int.TryParse(fieldSizeInputField.text, out var currentFieldSize))
+            {
+                currentFieldSize = 4;
+            }
+            
+            var generateId = $"game_types.random_match_{currentFieldSize}x{currentFieldSize}";
+            myDebugger.SimpleDebug($"getrandommatchref()...{generateId}");
+            randomMatchNameText.text = generateId;
+            return randomMatchRefs.FirstOrDefault(match => match.Id == generateId);
+        }
+
+        public async void OnSubscribeToRandomMatch()
+        {
+            SimGameTypeRef gameTypeRefToUse = null;
+            gameTypeRefToUse = GetRandomMatchRef();
+            _simGameType = await gameTypeRefToUse.Resolve();
+            
+            _data.SessionState = SessionState.Disconnected;
+            myDebugger.SimpleDebug($"Game Type = {_simGameType.Id}");
+            _myMatchmaking = new MyMatchMaking(_beamContext.Api.Experimental.MatchmakingService, 
+                _simGameType,
+                _beamContext.PlayerId,
+                myDebugger);
+            
+            _myMatchmaking.OnProgress.AddListener(MyMatchMaking_OnProgress);
+            _myMatchmaking.OnComplete.AddListener(MyMatchMaking_OnComplete);
+            _myMatchmaking.OnError.AddListener(MyMatchMaking_OnError);
+        }
+        
         private void MyMatchMaking_OnProgress(MyMatchMakingResult myMatchMakingResult)
         {
-            _myDebugger.SimpleDebug($"MyMatchMaking_OnProgress()...{myMatchMakingResult.ErrorMessage}");
+            myDebugger.SimpleDebug($"MyMatchMaking_OnProgress()...{myMatchMakingResult.ErrorMessage}");
             _data.SessionState = SessionState.Connecting;
         }
         
         private void MyMatchMaking_OnComplete(MyMatchMakingResult myMatchMakingResult)
         {
-            _myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...{myMatchMakingResult.MyMatchmakingHandle.Match.matchId}");
-            _myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...Teams = {myMatchMakingResult.MyMatchmakingHandle.Match.teams[0].name}");
-            _myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...Team player count {myMatchMakingResult.MyMatchmakingHandle.Match.teams[0].players.Count}");
+            myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...{myMatchMakingResult.MyMatchmakingHandle.Match.matchId}");
+            myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...Teams = {myMatchMakingResult.MyMatchmakingHandle.Match.teams[0].name}");
+            myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...Team player count {myMatchMakingResult.MyMatchmakingHandle.Match.teams[0].players.Count}");
             foreach (var player in myMatchMakingResult.MyMatchmakingHandle.Match.teams[0].players)
             {
-                _myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...Team player {player}");
+                myDebugger.SimpleDebug($"MyMatchMaking_OnComplete()...Team player {player}");
             }
             
             _data.SessionState = SessionState.Connected;
@@ -111,7 +158,7 @@ namespace MatchMaking
         
         private void MyMatchMaking_OnError(MyMatchMakingResult myMatchMakingResult)
         {
-            _myDebugger.SimpleDebug($"MyMatchMaking_OnError()...{myMatchMakingResult.ErrorMessage}");
+            myDebugger.SimpleDebug($"MyMatchMaking_OnError()...{myMatchMakingResult.ErrorMessage}");
             _data.SessionState = SessionState.Disconnected;
         }
 
@@ -124,24 +171,24 @@ namespace MatchMaking
             int.TryParse(powerStatValueInputField.text, out var powerStatValue);
             var service = _beamContext.Microservices().GameServer();
             service.SetPowerStat(powerStatString, powerStatValue.ToString());
-            _myDebugger.SimpleDebug($"{powerStatString} = {powerStatValue} for {_beamContext.PlayerId}");
+            myDebugger.SimpleDebug($"{powerStatString} = {powerStatValue} for {_beamContext.PlayerId}");
         }
         
 
         public async void StartMatchMaking()
         {
-            _myDebugger.SimpleDebug("Connecting to match...");
+            myDebugger.SimpleDebug("Connecting to match...");
             _data.SessionState = SessionState.Connecting;
             await _myMatchmaking.StartMatchMaking();
         }
         
         public async void CancelMatchMaking()
         {
-            _myDebugger.SimpleDebug("Disconnecting from match...");
+            myDebugger.SimpleDebug("Disconnecting from match...");
             _data.SessionState = SessionState.Disconnecting;
             await _myMatchmaking.CancelMatchMaking();
             
-            _myDebugger.SimpleDebug("Match Disconnected!");
+            myDebugger.SimpleDebug("Match Disconnected!");
             _data.SessionState = SessionState.Disconnected;
         }
 
